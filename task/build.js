0c3ee0955db36b2ea3bedf9cff5be3e4bcc52282
Noémie Lehuby <n.lehuby@qwant.com>
2017-12-14 15:04:26 +0100

    remove i18n fallbacks

--- task/build.js
+++ task/build.js
@@ -12,31 +12,8 @@ var slug = "qwantresearch/qwantmaps";
 var stylePath = '../style.json';
 var styleStr = fs.readFileSync(stylePath, 'utf8');
 
-var langCfgPath = '../lang-fallback.json';
-if (fs.existsSync(langCfgPath)) {
-  var langCfgStr = fs.readFileSync(langCfgPath, 'utf8');
-  var langCfg = JSON.parse(langCfgStr);
-}
-
 var style, outPath;
-if (langCfg) {
-  style = JSON.parse(styleStr);
-  transform.adjust_style_without_tilejson({
-    style: style,
-    needSprite: needSprite,
-    slug: slug,
-    langCfg: langCfg
-  }, [
-    "http://a.dev.tiles.maps.qwant.ninja:6533/oz/{z}/{x}/{y}.pbf",
-    "http://b.dev.tiles.maps.qwant.ninja:6533/oz/{z}/{x}/{y}.pbf",
-    "http://c.dev.tiles.maps.qwant.ninja:6533/oz/{z}/{x}/{y}.pbf"
-  ], [
-    "http://a.dev.tiles.maps.qwant.ninja:6533/poi/{z}/{x}/{y}.pbf",
-    "http://b.dev.tiles.maps.qwant.ninja:6533/poi/{z}/{x}/{y}.pbf",
-    "http://c.dev.tiles.maps.qwant.ninja:6533/poi/{z}/{x}/{y}.pbf"
-  ]);
-  fs.writeFileSync('build/style-dev.json', JSON.stringify(style, null, 2), 'utf8');
-}
+
 
 style = JSON.parse(styleStr);
 transform.adjust_style_without_tilejson({
@@ -52,19 +29,9 @@ transform.adjust_style_without_tilejson({
   "http://b.dev.tiles.maps.qwant.ninja:6533/poi/{z}/{x}/{y}.pbf",
   "http://c.dev.tiles.maps.qwant.ninja:6533/poi/{z}/{x}/{y}.pbf"
 ]);
-outPath = langCfg ? 'build/style-dev-undecorated.json' : 'build/style-dev.json';
+outPath = 'build/style-dev.json';
 fs.writeFileSync(outPath, JSON.stringify(style, null, 2), 'utf8');
 
-if (langCfg) {
-  style = JSON.parse(styleStr);
-  transform.adjustStyleForOpenMapTilesCDN({
-    style: style,
-    needSprite: needSprite,
-    slug: slug,
-    langCfg: langCfg
-  });
-  fs.writeFileSync('build/style-omt.json', JSON.stringify(style, null, 2), 'utf8');
-}
 
 style = JSON.parse(styleStr);
 transform.adjustStyleForOpenMapTilesCDN({
@@ -72,22 +39,9 @@ transform.adjustStyleForOpenMapTilesCDN({
   needSprite: needSprite,
   slug: slug
 });
-outPath = langCfg ? 'build/style-omt-undecorated.json' : 'build/style-omt.json';
+outPath =  'build/style-omt.json';
 fs.writeFileSync(outPath, JSON.stringify(style, null, 2), 'utf8');
-if (langCfg) {
-  style = JSON.parse(styleStr);
-  transform.adjust_style_without_tilejson({
-    style: style,
-    needSprite: needSprite,
-    slug: slug,
-    langCfg: langCfg
-  }, [
-    "http://localhost:6533/oz/{z}/{x}/{y}.pbf"
-  ], [
-    "http://localhost:6533/poi/{z}/{x}/{y}.pbf"
-  ]);
-  fs.writeFileSync('build/style-docker.json', JSON.stringify(style, null, 2), 'utf8');
-}
+
 
 style = JSON.parse(styleStr);
 transform.adjust_style_without_tilejson({
@@ -99,28 +53,10 @@ transform.adjust_style_without_tilejson({
 ], [
   "http://localhost:6533/poi/{z}/{x}/{y}.pbf"
 ]);
-outPath = langCfg ? 'build/style-docker-undecorated.json' : 'build/style-local.json';
+outPath =  'build/style-local.json';
 fs.writeFileSync(outPath, JSON.stringify(style, null, 2), 'utf8');
 
 
-if (langCfg) {
-  style = JSON.parse(styleStr);
-  transform.adjust_style_without_tilejson({
-    style: style,
-    needSprite: needSprite,
-    slug: slug,
-    langCfg: langCfg
-  }, [
-    "http://a.tiles.maps.qwant.com/oz/{z}/{x}/{y}.pbf",
-    "http://b.tiles.maps.qwant.com/oz/{z}/{x}/{y}.pbf",
-    "http://c.tiles.maps.qwant.com/oz/{z}/{x}/{y}.pbf"
-  ], [
-    "http://a.tiles.maps.qwant.com/poi/{z}/{x}/{y}.pbf",
-    "http://b.tiles.maps.qwant.com/poi/{z}/{x}/{y}.pbf",
-    "http://c.tiles.maps.qwant.com/poi/{z}/{x}/{y}.pbf"
-  ]);
-  fs.writeFileSync('build/style-prod.json', JSON.stringify(style, null, 2), 'utf8');
-}
 
 style = JSON.parse(styleStr);
 transform.adjust_style_without_tilejson({
@@ -136,5 +72,5 @@ transform.adjust_style_without_tilejson({
   "http://b.tiles.maps.qwant.com/poi/{z}/{x}/{y}.pbf",
   "http://c.tiles.maps.qwant.com/poi/{z}/{x}/{y}.pbf"
 ]);
-outPath = langCfg ? 'build/style-prod-undecorated.json' : 'build/style-prod.json';
+outPath = 'build/style-prod.json';
 fs.writeFileSync(outPath, JSON.stringify(style, null, 2), 'utf8');
