680fec38ca5c10b2b07ab26f47f210b52c850638
jlaville <j.laville@qwant.com>
2018-04-05 15:14:39 +0200

    add i18n config test

--- task/test.js
+++ task/test.js
@@ -1,8 +1,9 @@
-var fs = require('fs-extra');
-var mbgl = require('@mapbox/mapbox-gl-style-spec');
-var checkFonts = require('./check-fonts');
+const fs = require('fs-extra');
+const mbgl = require('@mapbox/mapbox-gl-style-spec');
+const checkFonts = require('./check-fonts');
 const path = require('path');
-var yargs = require('yargs');
+const yargs = require('yargs');
+const yaml = require('node-yaml')
 
 let args = yargs
   .usage('Usage: $0 [options]')
@@ -18,16 +19,18 @@ let args = yargs
   .alias('h', 'help')
   .argv;
 
-var stylePath = path.join(args.style_dir,'style.json');
-var styleString = fs.readFileSync(stylePath, 'utf8');
-var style = JSON.parse(styleString);
+const stylePath = path.join(args.style_dir,'style.json');
+const styleString = fs.readFileSync(stylePath, 'utf8');
+const style = JSON.parse(styleString);
 
-var errors = mbgl.validate(style);
+const errors = mbgl.validate(style);
 if(errors && errors.length) {
   console.error(
       'ERROR: The style is not valid according to mapbox-gl-style-spec.');
   console.error(errors);
   process.exit(1);
+} else {
+  console.log('✓ The style is valid according to mapbox-gl-style-spec.')
 }
 
 checkFonts(style);
@@ -42,9 +45,47 @@ if(!style.metadata['openmaptiles:version']) {
   console.log('WARNING: Style does not contain "openmaptiles:version" metadata.');
 }
 
-var formattedStyleString = mbgl.format(style);
+const formattedStyleString = mbgl.format(style);
 if(styleString.trim() !== formattedStyleString.trim()) {
   console.log(
-    'WARNING: Style is not formatted. You should use "gl-style-format"\n' +
+    'WARNING: Style is not correctly formatted. You should use "gl-style-format"\n' +
     'before commit. See https://github.com/mapbox/mapbox-gl-js/blob/master/src/style-spec/README.md');
+}
+
+
+/* Test i18n config file */
+
+const languageFallbacks = yaml.readSync(path.join(args.style_dir,'i18n.yml')).languageFallbacks
+
+const layersFields = [];
+let error = false;
+
+style.layers.map((layer) => {
+  if(layer['layout'] && layer['layout']['text-field'] && layer['layout']['text-field'] === '{name}') {
+    let fallback = languageFallbacks.find((languageFallback) => {
+      return languageFallback.id === layer.id
+    });
+
+    if(fallback) {
+      layersFields.push({status : true, id : layer.id, lang : fallback.lang.toString().substr(0,30) + '...'})
+    } else {
+      error = true;
+      layersFields.push({status : false, id : layer.id})
+    }
+  }
+});
+
+if(error) {
+  console.error('ERROR I18n file config is incomplete')
+  layersFields.forEach((layersField) => {
+    if(layersField.status) {
+      console.log(`✓ ${layersField.id} | ${layersField.lang}`)
+    } else {
+      console.error(`❌ ${layersField.id}`)
+    }
+  });
+
+  process.exit(1);
+} else {
+  console.log(`✓ i18n config file`)
 }
