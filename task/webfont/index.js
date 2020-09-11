24971edeeb2d87f2ca4ea4d891d47105f576a51f
bbecquet <benjamin.becquet@gmail.com>
2020-09-11 15:27:06 +0200

    Clean and simplify webfont task

--- task/webfont/index.js
+++ task/webfont/index.js
@@ -8,38 +8,35 @@ const path = require('path')
 const cleanSvgIcon = require('../../lib/clean_svg_icon');
 
 module.exports = async (options) => {
-  /* promisify */
   const readFile = util.promisify(fs.readFile)
   const readdir = util.promisify(fs.readdir)
-
-  let icons = await readdir(`${path.resolve(options.styleDir)}/icons`)
   const tmpSvgs = tmp.dirSync({unsafeCleanup : true})
 
-  /* clean circle form from SVG */
-  let iconsPromises = icons.filter((icon) => {
+  const iconFiles = await readdir(`${path.resolve(options.styleDir)}/icons`)
+
+  const cleanedIconsPromises = iconFiles
     /* use hi-res versions only */
-    return icon.match(/-15\.svg$/)
-  }).map((icon) => {
-    return new Promise(async (resolve, reject) => {
+    .filter(iconFile => iconFile.match(/-15\.svg$/))
+    .map(iconFile => new Promise(async (resolve, reject) => {
       try {
-        const svgStream = await readFile(`${path.resolve(options.styleDir)}/icons/${icon}`)
+        const svgStream = await readFile(`${path.resolve(options.styleDir)}/icons/${iconFile}`)
         const cleanSvg = await cleanSvgIcon(svgStream)
-        const iconName = icon.match(/^(.*?)-[0-9]{1,2}\.svg$/)[1]
-        fs.writeFileSync(`${tmpSvgs.name}/${iconName}.svg`, cleanSvg)
-        resolve(iconName)
+        const iconName = iconFile.match(/^(.*?)-[0-9]{1,2}\.svg$/)[1];
+        const iconPath = path.join(tmpSvgs.name,  `${iconName}.svg`);
+        fs.writeFileSync(iconPath, cleanSvg)
+        resolve(iconPath)
       } catch(e) {
         reject(e)
       }
-    })
-  })
+    }))
 
-  let cleanedIcons = await Promise.all(iconsPromises)
+  const cleanedIconFiles = await Promise.all(cleanedIconsPromises)
 
   /* build font */
   return new Promise((resolve, reject) => {
     webfontsGenerator({
       fontHeight : 150,
-      files: cleanedIcons.map((icon) => {return `${path.join(tmpSvgs.name, `${icon}.svg`)}`}),
+      files: cleanedIconFiles,
       dest: `${path.resolve(options.styleDir)}/build/font`
     }, (error, result) => {
       tmpSvgs.removeCallback()
