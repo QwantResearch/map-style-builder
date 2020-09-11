e62e1bea5fed1031ea953a0754d5339f3d4f59c7
bbecquet <benjamin.becquet@gmail.com>
2020-09-11 14:50:22 +0200

    Simplify WebFont task

--- task/webfont/index.js
+++ task/webfont/index.js
@@ -14,49 +14,47 @@ module.exports = async (options) => {
   const readdir = util.promisify(fs.readdir)
   const parseString = util.promisify(asyncParseString)
 
-  if(options.webfont) {
-    let icons = await readdir(`${path.resolve(options.styleDir)}/icons`)
-    const tmpSvgs = tmp.dirSync({unsafeCleanup : true})
+  let icons = await readdir(`${path.resolve(options.styleDir)}/icons`)
+  const tmpSvgs = tmp.dirSync({unsafeCleanup : true})
 
-    /* clean circle form from SVG */
-    let iconsPromises = icons.filter((icon) => {
-      /* clean size from svg */
-      return icon.match(/-15\.svg$/)
-    }).map((icon) => {
-      const xmlBuilder = new xml2js.Builder()
-      return new Promise(async (resolve, reject) => {
-        try {
-          let svgStream = await readFile(`${path.resolve(options.styleDir)}/icons/${icon}`)
-          let data = await parseString(svgStream)
-          delete data.svg.rect
-          const xml = xmlBuilder.buildObject(data)
-          let iconName = icon.match(/^(.*?)-[0-9]{1,2}\.svg$/)[1]
-          fs.writeFileSync(`${tmpSvgs.name}/${iconName}.svg`,xml)
-          resolve(iconName)
-        } catch(e) {
-          reject(e)
-        }
-      })
+  /* clean circle form from SVG */
+  let iconsPromises = icons.filter((icon) => {
+    /* clean size from svg */
+    return icon.match(/-15\.svg$/)
+  }).map((icon) => {
+    const xmlBuilder = new xml2js.Builder()
+    return new Promise(async (resolve, reject) => {
+      try {
+        let svgStream = await readFile(`${path.resolve(options.styleDir)}/icons/${icon}`)
+        let data = await parseString(svgStream)
+        delete data.svg.rect
+        const xml = xmlBuilder.buildObject(data)
+        let iconName = icon.match(/^(.*?)-[0-9]{1,2}\.svg$/)[1]
+        fs.writeFileSync(`${tmpSvgs.name}/${iconName}.svg`,xml)
+        resolve(iconName)
+      } catch(e) {
+        reject(e)
+      }
     })
+  })
 
-    let cleanedIcons = await Promise.all(iconsPromises)
+  let cleanedIcons = await Promise.all(iconsPromises)
 
-    /* build font */
-    return new Promise((resolve, reject) => {
-      webfontsGenerator({
-        fontHeight : 150,
-        files: cleanedIcons.map((icon) => {return `${path.join(tmpSvgs.name, `${icon}.svg`)}`}),
-        dest: `${path.resolve(options.styleDir)}/build/font`
-      }, (error, result) => {
-        tmpSvgs.removeCallback()
-        if (error) {
-          console.error('Error while building webfont!', error);
-          reject(err)
-        } else {
-          console.log(`Webfont built (${cleanedIcons.length} glyphs)`);
-          resolve()
-        }
-      })
+  /* build font */
+  return new Promise((resolve, reject) => {
+    webfontsGenerator({
+      fontHeight : 150,
+      files: cleanedIcons.map((icon) => {return `${path.join(tmpSvgs.name, `${icon}.svg`)}`}),
+      dest: `${path.resolve(options.styleDir)}/build/font`
+    }, (error, result) => {
+      tmpSvgs.removeCallback()
+      if (error) {
+        console.error('Error while building webfont!', error);
+        reject(err)
+      } else {
+        console.log(`Webfont built (${cleanedIcons.length} glyphs)`);
+        resolve()
+      }
     })
-  }
+  })
 }
